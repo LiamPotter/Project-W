ee80e387ab25e95727ea3dcdccad83c93c1f7dc4
LiamPotter <liamp62@gmail.com>
2016-11-07 16:40:39 +1000

    Added all of the relevant assets
    -Mainly just testing button configurations at the moment, will hook in
    some Database stuff later

---
+++ Assets/Scripts/CustomizationMenuManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class CustomizationMenuManager : MonoBehaviour {
+
+    public GameObject customizationPanel;
+
+    private List<Button> tabs = new List<Button>();
+
+    public Button activeButton;
+
+    public Button buttonPrefab;
+
+    public float panelMaxWidth;
+
+    public float panelMaxHeight;
+
+    public float heightPadding;
+
+    public float widthPadding;
+
+    public float amountOfRows;
+
+    public float amountPerRow;
+
+    public bool doneStart;
+	// Use this for initialization
+	void Start ()
+    {
+        foreach (Transform tab in customizationPanel.transform.GetComponentInChildren<Transform>())
+        {
+            if (tab.tag.Contains("Tab"))
+                tabs.Add(tab.GetComponent<Button>());
+        }
+        activeButton = tabs[0];
+        foreach (Button button in tabs)
+        {
+            if (button != activeButton)
+                button.GetComponent<TabVariables>().TurnOff();
+        }
+
+        panelMaxWidth = customizationPanel.GetComponent<RectTransform>().rect.width;
+        panelMaxHeight = customizationPanel.GetComponent<RectTransform>().rect.height;
+        amountPerRow = Mathf.Floor(panelMaxWidth / (buttonPrefab.targetGraphic.rectTransform.rect.width ));
+        amountOfRows = Mathf.Ceil(panelMaxHeight / (buttonPrefab.targetGraphic.rectTransform.rect.height + heightPadding)-1);
+
+
+        doneStart = true;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ApplyActiveButton(activeButton);
+        
+    }
+    private void SetActiveButton(Button toUse)
+    {
+        activeButton.GetComponent<TabVariables>().TurnOff();
+        activeButton.interactable = true;
+        activeButton = toUse;
+        activeButton.GetComponent<TabVariables>().TurnOn();
+    }
+    public void ApplyActiveButton(Button toUse)
+    {
+        SetActiveButton(toUse);
+        toUse.interactable = false;
+    }
+}
---
+++ Assets/Scripts/TabVariables.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class TabVariables : MonoBehaviour {
+
+    private CustomizationMenuManager cMenuManager;
+
+    public GameObject holderObject;
+
+    private GameObject buttonHolder;
+
+    public List<GameObject> possibleSelections = new List<GameObject>();
+
+    private List<Button> relatedButtons = new List<Button>();
+
+    private bool doneStart =false;
+    
+    private int amountOfLines;
+
+	void Start ()
+    {
+        cMenuManager = FindObjectOfType<CustomizationMenuManager>();
+        buttonHolder = holderObject.transform.FindChild("ButtonHolder").gameObject;
+    }
+	void Update()
+    {
+        if (cMenuManager.doneStart)
+            if (!doneStart)
+                RunStart();
+    }
+    private float IfOverForY(int i,int greaterThan, float returnFloat)
+    {
+        if (i > greaterThan)
+        {
+            return returnFloat;
+        }
+        else return 0f;
+    }
+    private float IfOverForX(int i,int greaterThan,float returnFloat)
+    {
+        if (i > greaterThan)
+        {
+            return returnFloat;
+        }
+        else return 0f;
+    }
+    public void TurnOff()
+    {
+        holderObject.SetActive(false);
+    }
+    public void TurnOn()
+    {
+        holderObject.SetActive(true);
+    }
+    private void RunStart()
+    {
+      
+        amountOfLines = possibleSelections.Count / (int)cMenuManager.amountPerRow;
+        if (possibleSelections.Count % cMenuManager.amountPerRow > 0)
+            amountOfLines++;
+        buttonHolder.GetComponent<GridLayoutGroup>().cellSize = new Vector2(cMenuManager.buttonPrefab.targetGraphic.rectTransform.rect.width, cMenuManager.buttonPrefab.targetGraphic.rectTransform.rect.height);
+        for (int i = 0; i < possibleSelections.Count; i++)
+        {
+          
+               // if (relatedButtons.Count < possibleSelections.Count)
+            relatedButtons.Add((Button)Instantiate(cMenuManager.buttonPrefab, new Vector3(
+                cMenuManager.customizationPanel.transform.position.x,
+                cMenuManager.customizationPanel.transform.position.y,
+                cMenuManager.customizationPanel.transform.position.z),
+                cMenuManager.customizationPanel.transform.rotation,
+                buttonHolder.transform));
+        }
+        doneStart = true;
+    }
+}
